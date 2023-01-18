db5888bfb180ee3d0532d43b58caa6323e4e6297
Jose Guerrero Serrano <jose.guerrero@sofka.com.co>
2023-01-18 14:54:41 -0500

    Feature: add Title atom.

--- src/app/components/atoms/atoms.module.ts
+++ src/app/components/atoms/atoms.module.ts
@@ -4,9 +4,10 @@ import { ReactiveFormsModule } from '@angular/forms';
 
 import { IconComponent } from './icon/icon.component';
 import { TextInputComponent } from './text-input/text-input.component';
+import { TitleComponent } from './title/title.component';
 
 @NgModule({
-	declarations: [IconComponent, TextInputComponent],
+	declarations: [IconComponent, TextInputComponent, TitleComponent],
 	imports: [CommonModule, ReactiveFormsModule],
 	exports: [IconComponent, TextInputComponent],
 })
