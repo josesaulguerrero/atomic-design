d64e8c4378eb66e9ea1e4994cc58618d2fd1aa45
Jose Guerrero Serrano <jose.guerrero@sofka.com.co>
2023-01-18 15:05:40 -0500

    Fix: add missing exports in Atoms module.

--- src/app/components/atoms/atoms.module.ts
+++ src/app/components/atoms/atoms.module.ts
@@ -4,12 +4,17 @@ import { ReactiveFormsModule } from '@angular/forms';
 
 import { IconComponent } from './icon/icon.component';
 import { TextInputComponent } from './text-input/text-input.component';
-import { TitleComponent } from './title/title.component';
 import { TextComponent } from './text/text.component';
+import { TitleComponent } from './title/title.component';
 
 @NgModule({
-	declarations: [IconComponent, TextInputComponent, TitleComponent, TextComponent],
+	declarations: [
+		IconComponent,
+		TextInputComponent,
+		TitleComponent,
+		TextComponent,
+	],
 	imports: [CommonModule, ReactiveFormsModule],
-	exports: [IconComponent, TextInputComponent],
+	exports: [IconComponent, TextInputComponent, TitleComponent, TextComponent],
 })
 export class AtomsModule {}
