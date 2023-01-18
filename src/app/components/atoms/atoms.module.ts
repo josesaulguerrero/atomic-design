dc872cb06159e542dd5a08447126abc4f0d25e2f
Jose Guerrero Serrano <jose.guerrero@sofka.com.co>
2023-01-18 14:58:29 -0500

    Feature: add Text atom.

--- src/app/components/atoms/atoms.module.ts
+++ src/app/components/atoms/atoms.module.ts
@@ -5,9 +5,10 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { IconComponent } from './icon/icon.component';
 import { TextInputComponent } from './text-input/text-input.component';
 import { TitleComponent } from './title/title.component';
+import { TextComponent } from './text/text.component';
 
 @NgModule({
-	declarations: [IconComponent, TextInputComponent, TitleComponent],
+	declarations: [IconComponent, TextInputComponent, TitleComponent, TextComponent],
 	imports: [CommonModule, ReactiveFormsModule],
 	exports: [IconComponent, TextInputComponent],
 })
