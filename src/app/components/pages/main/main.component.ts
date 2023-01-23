af102334e41e2ca7b27635d72789016f171e30cb
Jose Guerrero Serrano <jose.guerrero@sofka.com.co>
2023-01-23 14:27:26 -0500

    Fix: remove fixed width from sidebar container.

--- src/app/components/pages/main/main.component.ts
+++ src/app/components/pages/main/main.component.ts
@@ -1,15 +1,22 @@
+import { Chat } from 'src/app/modules/core/models/chat.model';
+
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-main',
-  templateUrl: './main.component.html',
-  styleUrls: ['./main.component.scss']
+	selector: 'app-main',
+	templateUrl: './main.component.html',
+	styleUrls: ['./main.component.scss'],
 })
 export class MainComponent implements OnInit {
+	public chat: Chat = {
+		details: {
+			avatar: 'https://pbs.twimg.com/media/D8dDZukXUAAXLdY.jpg',
+			lastTimeOnline: new Date(),
+			name: 'Alicia Torreaux',
+		},
+	};
 
-  constructor() { }
+	constructor() {}
 
-  ngOnInit(): void {
-  }
-
+	ngOnInit(): void {}
 }
