6b6ddbe18eb670dfafef1a297b90cb658c98bcbe
Jose Guerrero Serrano <jose.guerrero@sofka.com.co>
2023-01-24 15:58:27 -0500

    Feature: add Auth Button Atom.

--- src/app/components/atoms/atoms.module.ts
+++ src/app/components/atoms/atoms.module.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 
+import { AuthButtonComponent } from './auth-button/auth-button.component';
 import { ClickableIconComponent } from './clickable-icon/clickable-icon.component';
 import { IconComponent } from './icon/icon.component';
 import { InputLabelComponent } from './input-label/input-label.component';
@@ -17,6 +18,7 @@ import { TitleComponent } from './title/title.component';
 		TextComponent,
 		ClickableIconComponent,
 		InputLabelComponent,
+		AuthButtonComponent,
 	],
 	imports: [CommonModule, ReactiveFormsModule],
 	exports: [
@@ -26,6 +28,7 @@ import { TitleComponent } from './title/title.component';
 		TextComponent,
 		ClickableIconComponent,
 		InputLabelComponent,
+		AuthButtonComponent,
 	],
 })
 export class AtomsModule {}
---
+++ src/app/components/atoms/auth-button/auth-button.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input } from '@angular/core';
+
+type AuthPlatform = {
+	name: string;
+	iconSrc: string;
+};
+
+@Component({
+	selector: 'app-auth-button',
+	templateUrl: './auth-button.component.html',
+	styleUrls: ['./auth-button.component.scss'],
+})
+export class AuthButtonComponent {
+	@Input()
+	public authPlatform!: AuthPlatform;
+
+	public constructor() {}
+}
